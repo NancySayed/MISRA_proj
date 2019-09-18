dfd8bfe5d8d24921bd7f85fdebf25b73f87c0575
NancySayed <54311678+NancySayed@users.noreply.github.com>
2019-09-18 18:46:36 +0200

    Editting (#1)
    * BLMGR file
    
    * BLMGR_CFG file
    
    * BLTD file
    
    * CRC file
    
    * keypad file
    
    * L3G4200D_cfg file
    
    * uart_test file
    
    * L3G4200D file

--- L3G4200D_Lcfg.c
+++ L3G4200D_Lcfg.c
@@ -9,48 +9,33 @@
 #include "L3G4200D_Lcfg.h"
 #include "L3G4200D.h"
 
-#define     VALUE_1     ( (u8) 0x01 )
+void GYHD_INIT_SLAVE_SELECT(void)
+{
+    DIO_InitPortDirection(PB,(1u)<<4,(1u)<<4);
+    DIO_WritePort(PB,(1u)<<4,(1u)<<4);
+}
 
-#if(u8TIMEOUT_FUNCTION == u8USE_DELAY)
-void u8START_TIME_OUT_MS(u8 DELAY_MS, u8 * FLAG_PTR)
+void GYHD_ACTIVATE_SLAVE_SELECT(void)
+{
+    u8 data = 1u;
+    DIO_WritePort(PB,(((u8)(~data))<<4),(1u)<<4);
+}
+
+void GYHD_DEACTIVATE_SLAVE_SELECT(void)
+{
+    DIO_WritePort(PB,((1u)<<4),((1u)<<4));
+}
+
+void u8START_TIME_OUT_MS(u32 DELAY_MS,u8 * FLAG_PTR)
 {
     _delay_ms(DELAY_MS);
-     *(FLAG_PTR) = VALUE_1;
+    *(FLAG_PTR) = 0x01u;
 }
-/*#define u8START_TIME_OUT_MS(DELAY_MS,FLAG_PTR) do { _delay_ms(DELAY_MS); *(FLAG_PTR) = 0x01 } while(0)*/
-#else
-#define u8START_TIME_OUT_MS(DELAY_MS,FLAG_PTR)
-#endif
 
 
-const L3G4200D_CfgType L3G4200D_ConfigParam = {
-                                                u8FS_250,
-                                                {u8X_AXIS_ACTIVE, u8Y_AXIS_ACTIVE, u8Z_AXIS_ACTIVE},
-                                                {u8NO_FILTERS, u8NO_FILTERS},
-                                                {0X2CA4, 0x2CA4, 0x2CA, u8INT1_X, u8INT1_Y, u8INT1_Z, 0x00 , 0x00 }
-                                               };
+const L3G4200D_CfgType L3G4200D_ConfigParam = { {u8FS_250},
+                                                {u8X_AXIS_ACTIVE,u8Y_AXIS_ACTIVE,u8Z_AXIS_ACTIVE},
+                                                {u8NO_FILTERS,u8NO_FILTERS},
+                                                {0X2CA4,0x2CA4,0x2CA4,u8INT1_X,u8INT1_Y,u8INT1_Z,0x00,0x00}
+};
 
-
-
-/*
-const L3G4200D_CfgType L3G4200D_ConfigParam = {
-                                                u8FS_250,
-
-                                               .strAxisActivation.u8XAxisActivation    = u8X_AXIS_ACTIVE,
-											   .strAxisActivation.u8YAxisActivation    = u8Y_AXIS_ACTIVE,
-											   .strAxisActivation.u8ZAxisActivation    = u8Z_AXIS_ACTIVE,
-
-											   .strFilterCfg.u8FilterOnData            = u8NO_FILTERS,
-											   .strFilterCfg.u8FilterOnInterrupt       = u8NO_FILTERS,
-
-											   .strIntCfg.u16ThresholdX                = 0X2CA4,
-											   .strIntCfg.u16ThresholdY                = 0x2CA4,
-											   .strIntCfg.u16ThresholdZ                = 0x2CA,
-											   .strIntCfg.u8XAxisInterrupt             = u8INT1_X,
-											   .strIntCfg.u8YAxisInterrupt             = u8INT1_Y,
-											   .strIntCfg.u8ZAxisInterrupt             = u8INT1_Z,
-											   .strIntCfg.u8AOICfg                     = 0x00,
-											   .strIntCfg.u8IntDuration                = 0x00
-											   };
-
-*/
